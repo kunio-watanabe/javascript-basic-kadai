dc66b1db6cd3c26842581a776cdfd838cd8ff567
kunio.watanabe <0712yumika@gmail.com>
2025-04-05 15:10:49 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,1 +1,9 @@
-console.log( 68 / ( 1.7 * 1.7 ) )
+// ① 体重と身長を変数に入れる
+let weight = 68;      // 体重（kg）
+let height = 1.7;     // 身長（m）
+
+// ② BMIを計算して、変数bmiに代入
+let bmi = weight / (height * height);
+
+// ③ 結果をコンソールに表示
+console.log(bmi);
